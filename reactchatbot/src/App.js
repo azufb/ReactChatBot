ec1c0244969871686ce517463893bb0b5e5ca0a3
azusa okamoto <curiousazunas@gmail.com>
2020-07-17 13:50:05 +0900

    依存関係の変更

--- reactchatbot/src/App.js
+++ reactchatbot/src/App.js
@@ -5,8 +5,6 @@ import CustomChatbot from './chatbot/CustomChatbot';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
       <CustomChatbot />
     </div>
   );
